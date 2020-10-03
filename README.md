680a187f51e540c3040a0ef5889b69413d80894b
romanver <romanver777@yandex.ru>
2020-10-03 22:47:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Сборка работает на gulp версии 4.0. 
+
+1.npm i gulpjs/gulp-cli -g
+
+2.npm install
+3.run gulp
---
+++ source/js/app.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  setTimeout(() => {
+
+  	document.querySelector('.greating__text').classList.add('visible');
+  }, 1000);
+})();
