a3228e3b83fbdf4db8c0d5e3f101fcb2e903cbe8
romanver <romanver777@yandex.ru>
2020-10-13 11:45:21 +0300

    Добавлено открытие/закрытие menu-mobile

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,26 @@
 (function() {
 
+	(function() {
+
+		const mobileMenu = document.querySelector('.mobile-menu');
+		const container = document.querySelector('.container');
+		const menuOpenLink = document.querySelector('.menu__btn_gamburger');
+		const menuCloseLink = document.querySelector('.mobile-menu__btn_cross');
+
+		menuOpenLink.addEventListener('click', (e) => {
+
+			e.preventDefault();
+			mobileMenu.classList.add('mobile-menu_show');
+			container.classList.add('container_margin-top');
+		});
+
+		menuCloseLink.addEventListener('click', (e) => {
+
+			e.preventDefault();
+			mobileMenu.classList.remove('mobile-menu_show');
+			container.classList.remove('container_margin-top');
+		});
+
+	})();
+
 })();
