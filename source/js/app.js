af0b730c6357edcfb55cd1f1b07fba252510262d
romanver <romanver777@yandex.ru>
2020-10-16 21:06:59 +0300

    добавлен photo mobile

--- source/js/app.js
+++ source/js/app.js
@@ -30,32 +30,37 @@
 
 	(function (){
 
-		const token = 'pk.eyJ1IjoiY29udHJhNzciLCJhIjoiY2p0anR2MWEwMnZrMjN5cDhsbXczeDJ1cSJ9.Q9r4aUzdARiggKd4gEmFDA';
-		const mapStyle = 'mapbox://styles/contra77/ckg92q4wq09fs19n84xqpbb8n';
+		const mapSection = document.querySelector('.map');
 
-		const mapCenterCoords = [-111.798486, 34.757056];
-		const mapMarkerCoords = [-111.798486, 34.863156];
-		const mapMarkerColor = '#81b3d2';
-		const mapDiv = 'map';
-		const zoom = 7.3;
+		if(mapSection) {
 
-		mapboxgl.accessToken = token;
+			const token = 'pk.eyJ1IjoiY29udHJhNzciLCJhIjoiY2p0anR2MWEwMnZrMjN5cDhsbXczeDJ1cSJ9.Q9r4aUzdARiggKd4gEmFDA';
+			const mapStyle = 'mapbox://styles/contra77/ckg92q4wq09fs19n84xqpbb8n';
 
-		let map = new mapboxgl.Map({
-			container: mapDiv,
-			style: mapStyle,
-			center: mapCenterCoords,
-			zoom: zoom
-		});
-		// запрет зума мышью
-		map.scrollZoom.disable();
-		// кнопки зума
-		// 	map.addControl(new mapboxgl.NavigationControl(), 'top-left');
-		// }
+			const mapCenterCoords = [-111.798486, 34.757056];
+			const mapMarkerCoords = [-111.798486, 34.863156];
+			const mapMarkerColor = '#81b3d2';
+			const mapDiv = 'map';
+			const zoom = 7.3;
 
-		let marker = new mapboxgl.Marker({ color: mapMarkerColor })
-			.setLngLat(mapMarkerCoords).addTo(map);
+			mapboxgl.accessToken = token;
 
+			let map = new mapboxgl.Map({
+				container: mapDiv,
+				style: mapStyle,
+				center: mapCenterCoords,
+				zoom: zoom
+			});
+			// запрет зума мышью
+			map.scrollZoom.disable();
+			// кнопки зума
+			// 	map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+			// }
+
+			let marker = new mapboxgl.Marker({color: mapMarkerColor})
+				.setLngLat(mapMarkerCoords).addTo(map);
+
+		}
 	})();
 
 })();
